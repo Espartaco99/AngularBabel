6ae05766d817865f89e333b5f5ba766c6b845d3a
Jesus <jperucha@ucm.es>
2018-03-08 09:09:19 +0100

    Creado y añadido al menu el componente con lazy routing agenda

---
+++ base-angular/src/app/agenda/agenda.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-agenda',
+  template: `
+    <p>
+      agenda works!
+    </p>
+  `,
+  styles: []
+})
+export class AgendaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
